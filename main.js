828e29b0d9d114eaafc983499c157b958272b8a4
Juan Osorio <andesignerx@gmail.com>
2021-07-28 15:57:01 -0500

    --

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Int8Attribute } from 'three';
+import * .jpg from 'dsignerx.github.io/';
 
 // Setup
 
