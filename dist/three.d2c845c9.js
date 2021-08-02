d12a1fa672fbab569e57fd04b50c19416024a874
Juan Osorio <andesignerx@gmail.com>
2021-08-02 18:22:37 -0500

    --

--- dist/three.d2c845c9.js
+++ dist/three.d2c845c9.js
@@ -28855,7 +28855,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49307" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63755" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
